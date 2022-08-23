f8bd5643927e6097ef7133044b368a6d40f030d5
あなたの名前 <>
2022-08-23 09:02:10 +0900

    completed login function

--- src/app/Http/Controllers/WebappController.php
+++ src/app/Http/Controllers/WebappController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\Post;
 use App\Connect;
@@ -10,22 +11,44 @@ use DateTime;
 
 class WebappController extends Controller
 {
-    public function index($user_id)
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        $user = User::where('id', $user_id)->get();
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    // public function index()
+    // {
+    //     return view('webapp');
+    // }
+
+    public function show()
+    {
+
+        $user_id=Auth::user()->id;
+        $user = User::where('id',  $user_id)->get();
 
         //今日の勉強時間
-        $today_study_hours = Post::where('user_id', $user_id)->whereDate('day', date('Y-m-d'))->sum('hour');
+        $today_study_hours = Post::where('user_id',  $user_id)->whereDate('day', date('Y-m-d'))->sum('hour');
         // dd($today_study_hours);
 
         //今月の勉強時間
-        $month_study_hours = Post::where('user_id', $user_id)->whereYear('day', date('Y'))->whereMonth('day', date('m'))->sum('hour');
+        $month_study_hours = Post::where('user_id',  $user_id)->whereYear('day', date('Y'))->whereMonth('day', date('m'))->sum('hour');
 
         //今年の勉強時間
-        $total_study_hours = Post::where('user_id', $user_id)->sum('hour');
+        $total_study_hours = Post::where('user_id',  $user_id)->sum('hour');
 
         return view('webapp', [
-            'user_id' => $user_id, 'user' => $user,
+            'user_id' =>  $user_id, 'user' => $user,
             'today_study_hours' => $today_study_hours, 'month_study_hours' => $month_study_hours,
             'total_study_hours' => $total_study_hours
         ]);
