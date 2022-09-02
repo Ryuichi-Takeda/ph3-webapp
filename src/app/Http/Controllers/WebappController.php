5f0fb9c663826d8d01265cadd3f2645e5c696e73
あなたの名前 <>
2022-09-02 11:45:36 +0900

    wrote commets

--- src/app/Http/Controllers/WebappController.php
+++ src/app/Http/Controllers/WebappController.php
@@ -24,10 +24,21 @@ class WebappController extends Controller
         //今年の勉強時間
         $total_study_hours = Post::where('user_id', $user_id)->sum('hour');
 
+
+        // $day_study_hours = [];
+        for($i=0;$i<=31;$i++){
+        // $day_study_hour = Post::where('user_id', $user_id)->whereYear('day', date('Y'))->whereMonth('day', date('m'))
+        // ->whereDay('day',$i)->sum('hour');
+        // array_push($day_study_hours,$day_study_hour);
+        }
+        //modelでうんたんしてcontrollerで取得する
+        // dd($day_study_hours_[17]);
+
         return view('webapp', [
             'user_id' => $user_id, 'user' => $user,
             'today_study_hours' => $today_study_hours, 'month_study_hours' => $month_study_hours,
-            'total_study_hours' => $total_study_hours
+            'total_study_hours' => $total_study_hours,
+            'day_study_hours' => $day_study_hours
         ]);
     }
 }
