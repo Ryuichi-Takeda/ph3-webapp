221221309d7bcb70619e5e9668902564393cfcca
あなたの名前 <>
2022-10-16 18:43:06 +0900

    deleted js the files for graphs and brought the contents in the blade file

--- src/app/Http/Controllers/StudyDataController.php
+++ src/app/Http/Controllers/StudyDataController.php
@@ -21,24 +21,22 @@ class StudyDataController extends Controller
         //今月の勉強時間
         $month_study_hours = Post::where('user_id', $user_id)->whereYear('day', date('Y'))->whereMonth('day', date('m'))->sum('hour');
 
-        //今年の勉強時間
+        //総勉強時間
         $total_study_hours = Post::where('user_id', $user_id)->sum('hour');
 
 
-        // $day_study_hours = [];
-        for($i=0;$i<=31;$i++){
-        // $day_study_hour = Post::where('user_id', $user_id)->whereYear('day', date('Y'))->whereMonth('day', date('m'))
-        // ->whereDay('day',$i)->sum('hour');
-        // array_push($day_study_hours,$day_study_hour);
-        }
-        //modelでうんたんしてcontrollerで取得する
-        // dd($day_study_hours_[17]);
+        // // $day_study_hours = [];
+        // for($i=0;$i<=31;$i++){
+        // // $day_study_hour = Post::where('user_id', $user_id)->whereYear('day', date('Y'))->whereMonth('day', date('m'))
+        // // ->whereDay('day',$i)->sum('hour');
+        // // array_push($day_study_hours,$day_study_hour);
+        // }
+        
 
         return view('webapp', [
             'user_id' => $user_id, 'user' => $user,
             'today_study_hours' => $today_study_hours, 'month_study_hours' => $month_study_hours,
-            'total_study_hours' => $total_study_hours,
-            'day_study_hours' => $day_study_hours
+            'total_study_hours' => $total_study_hours
         ]);
     }
 }
