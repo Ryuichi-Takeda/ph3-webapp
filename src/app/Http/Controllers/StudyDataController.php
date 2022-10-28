a8713e455bd2d2c3455e816d249b74b628a28515
あなたの名前 <>
2022-10-29 04:30:22 +0900

    completed the login function

--- src/app/Http/Controllers/StudyDataController.php
+++ src/app/Http/Controllers/StudyDataController.php
@@ -11,6 +11,23 @@ use DateTime;
 
 class StudyDataController extends Controller
 {
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+
     public function index()
     {
         $user_id = Auth::user()->id;
